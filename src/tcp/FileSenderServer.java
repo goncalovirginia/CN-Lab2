7d62e60b68c1b8a2d87c154e12916f34b3593054
Gonçalo <gonvirginia.blueslime@gmail.com>
2021-10-15 14:29:52 +0100

    Initial commit

---
+++ src/tcp/FileSenderServer.java
@@ -0,0 +1,39 @@
+package tcp;
+
+import java.io.*;
+import java.net.*;
+
+public class FileSenderServer {
+	static final int BUF_SIZE = 1024;
+	public static int PORT = 8000;
+
+	public static String readLine(InputStream is) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		
+		// TODO
+		
+		return sb.toString();
+	}
+
+	static void receiveFile(Socket cs) throws IOException {
+
+		// TODO
+		
+	}
+
+	public static void main(String[] args) {
+		try (ServerSocket ss = new ServerSocket(PORT)) {
+			for (;;) {
+				try (Socket cs = ss.accept()) {
+
+					receiveFile(cs);
+
+				} catch (IOException x) {
+					x.printStackTrace();
+				}
+			}
+		} catch (IOException x) {
+			x.printStackTrace();
+		}
+	}
+}
