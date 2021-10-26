a7ccd20a5f050f58d3a4f067e7274db9ac4d2fdc
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2021-10-26 23:21:08 +0100

    complete

--- src/tcp/FileSenderClient.java
+++ src/tcp/FileSenderClient.java
@@ -10,18 +10,18 @@ public class FileSenderClient {
 	private static final int BUF_SIZE = 1024;
 
 	static void sendFile(String server, int port, String filename) {
-
 		try (Socket socket = new Socket(server, port); FileInputStream fis = new FileInputStream(filename)) {
-
 			OutputStream os = socket.getOutputStream();
 			
-			os.write( filename.getBytes());
-			os.write( NEWLINE );
+			os.write(filename.getBytes());
+			os.write(NEWLINE);
 
 			int n;
 			byte[] buf = new byte[BUF_SIZE];
-			while ((n = fis.read(buf)) > 0)
+			
+			while ((n = fis.read(buf)) > 0) {
 				os.write(buf, 0, n);
+			}
 			
 		} catch (IOException x) {
 			x.printStackTrace();
--- src/tcp/FileSenderServer.java
+++ src/tcp/FileSenderServer.java
@@ -2,6 +2,7 @@ package tcp;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 public class FileSenderServer {
 	static final int BUF_SIZE = 1024;
@@ -9,25 +10,35 @@ public class FileSenderServer {
 
 	public static String readLine(InputStream is) throws IOException {
 		StringBuffer sb = new StringBuffer();
+		String buff;
 		
-		// TODO
+		while (!(buff = new String(is.readNBytes(1))).equals("\n")) {
+			sb.append(buff);
+		}
 		
 		return sb.toString();
 	}
 
 	static void receiveFile(Socket cs) throws IOException {
-
-		// TODO
+		InputStream is = cs.getInputStream();
+		String filename = readLine(is);
 		
+		try (FileOutputStream fos = new FileOutputStream( "Copy-of-" + filename)) {
+			byte[] fileData = is.readAllBytes();
+			fos.write(fileData);
+		}
+		catch (Exception x) {
+			x.printStackTrace();
+		}
+		
+		cs.close();
 	}
 
 	public static void main(String[] args) {
 		try (ServerSocket ss = new ServerSocket(PORT)) {
 			for (;;) {
 				try (Socket cs = ss.accept()) {
-
 					receiveFile(cs);
-
 				} catch (IOException x) {
 					x.printStackTrace();
 				}
